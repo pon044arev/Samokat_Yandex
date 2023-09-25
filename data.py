f855f6cb9155546694a8e1d0d76d10840cce6757
pon044arev <pon044arev.v@yandex.ru>
2023-09-25 21:11:00 +0700

    Добавление файлов

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Влад",
+    "lastName": "Пономарев",
+    "address": "Лубянка, 5",
+    "metroStation": 7,
+    "phone": "+79998887766",
+    "rentTime": 2,
+    "deliveryDate": "2023-09-30",
+    "comment": "Звонить заранее",
+    "color": [
+        "BLACK"
+    ]
+}
